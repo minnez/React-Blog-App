cbf45e459bd770fe86d99ac57319905cec4eb0ba
minnez <patrickackom003@gmail.com>
2022-08-23 19:51:15 +0000

    changed to hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './styles/App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
 import Listview from './views/Listview';
 import Aboutview from './views/Aboutview';
