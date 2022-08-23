269e0434228cae0f69092ecad36d7c1a3d7013c9
minnez <patrickackom003@gmail.com>
2022-08-23 17:20:11 +0000

    pushed a config file

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const config = {
+"REACT_APP_API_KEY" : "AIzaSyB_8QqWsLKoKVu00u6R6G2KUmAKzU6ZWFM",
+"REACT_APP_AUTH_DOMAIN": "reactfirebaseblog-eee65.firebaseapp.com",
+"REACT_APP_PROJECT_ID": "reactfirebaseblog-eee65",
+"REACT_APP_STORAGE_BUCKET": "reactfirebaseblog-eee65.appspot.com",
+"REACT_APP_MESSAGING_SENDER_ID": "409058107724",
+"REACT_APP_APP_ID" :"1:409058107724:web:41cb02cdfd106fbe872fe9"
+}
+export default config
+
