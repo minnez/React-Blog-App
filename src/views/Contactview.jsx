3ba4c2de316114d4ee2676bcb5a32d493fc5babc
minnez <patrickackom003@gmail.com>
2022-08-23 19:24:25 +0000

    checks

--- src/views/Contactview.jsx
+++ src/views/Contactview.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 const Contactview = () => {
-        // console.log("contactview.js")
+        console.log("contactview.js")
     return ( 
         <h2>Contactview</h2>
      );
